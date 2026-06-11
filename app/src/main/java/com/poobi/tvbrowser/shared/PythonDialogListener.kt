167d4d7772cfda24a53c140434fb2ebce7386328
Chris Malone <chrismrulz@hotmail.com>
2026-06-12 00:59:30 +1000

    include upcoming captcha functions

--- app/src/main/java/com/poobi/tvbrowser/shared/PythonDialogListener.kt
+++ app/src/main/java/com/poobi/tvbrowser/shared/PythonDialogListener.kt
@@ -4,6 +4,8 @@ interface PythonDialogListener {
     fun infoDialog(message: String, heading: String, sound: Boolean, icon: String)
     fun okDialog(message: String, heading: String): Boolean
     fun yesnoDialog(message: String, heading: String, nolabel: String, yeslabel: String): Boolean
+    fun selectDialog(options: List<String>, heading: String): Int
+    fun captchaDialog(imageBytes: ByteArray, heading: String): String?
 }
 
 fun String.cleanKodiText(): String = this.replace("[CR]", "\n").replace("[B]", "").replace("[/B]", "")
